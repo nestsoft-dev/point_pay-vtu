3c61b756878ba371ca73a706127b38c5ec36f968
IKENNA COLLINS <obettaikenna19@gmail.com>
2023-06-26 11:37:30 +0100

    first commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static Color myGreen = const Color.fromARGB(255, 29, 167, 33);
+  static Color? background =  Colors.grey[200];
+ // static Color myGreen = const Color.fromARGB(255, 29, 167, 33);
+ static String API_KEY = '';
+ //static String API_KEY = '';
+}
